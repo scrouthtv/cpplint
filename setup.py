b9ff3c336b59a6a26042e1e08cb75698f1c4b4e2
Aaron Liu <aaronliu0130@gmail.com>
2024-02-03 19:41:19 -0500

    add back 3.7

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(name='cpplint',
                    'License :: Freely Distributable'
                    'Natural Language :: English',
                    'Programming Language :: Python :: 3 :: Only',
+                   'Programming Language :: Python :: 3.7',
                    'Programming Language :: Python :: 3.8',
                    'Programming Language :: Python :: 3.9',
                    'Programming Language :: Python :: 3.10',
